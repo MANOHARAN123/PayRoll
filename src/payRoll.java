20798be6df6292f8ba055a02eb31b46563ec6146
kamesh <kameshkumaar12612@gmail.com>
2023-07-10 16:53:25 +0530

    third commit

---
+++ src/payRoll.java
@@ -0,0 +1,26 @@
+
+class SalariedEmploye
+{
+
+    void calculatePay(String  name,double salary){
+        System.out.println("Name"+name+"\nsalary "+salary);
+
+    }
+
+}
+class HourlyEmployee{
+    String name;
+    double hourlyRated;
+    double hoursWorked;
+    void calculate(String name,double hourlyRated,double hoursWorked)
+    {
+        System.out.println("Name "+name);
+    }
+
+}
+
+
+public class payRoll {
+
+
+}
